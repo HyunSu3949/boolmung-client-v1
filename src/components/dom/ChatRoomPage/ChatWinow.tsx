f6a2654162026288113d77a4febe84c274fdbf08
hyunsu.An <ahs39490215@gmail.com>
2024-03-10 18:58:29 +0900

    refactor: virtual scrolling으로 렌더링 퍼포먼스 개선

--- src/components/dom/ChatRoomPage/ChatWinow.tsx
+++ src/components/dom/ChatRoomPage/ChatWinow.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
-import { ChatList } from "src/components/dom/ChatRoomPage/ChatList";
+import { MessageList } from "src/components/dom/ChatRoomPage/MessageList";
 import { RootState } from "src/redux/store";
 import MessageInput from "src/components/dom/ChatRoomPage/MessageInput";
 
@@ -15,16 +15,18 @@ export function ChatWindow() {
   return (
     <div
       ref={chatWindowRef}
-      className="flex w-full flex-1 flex-col bg-gray-900 p-2 text-white"
+      className="flex flex-col flex-1 w-full p-2 text-white bg-gray-900"
     >
-      <div className="flex w-full flex-1 overflow-auto rounded-lg border border-gray-700 bg-gray-800">
-        <ChatList />
+      <div className="flex w-full h-full overflow-auto bg-gray-800 border border-gray-700 rounded-lg">
+        <MessageList />
       </div>
-      <MessageInput
-        userId={user._id}
-        userName={user.name}
-        roomId={roomid as string}
-      />
+      <div className="h-15">
+        <MessageInput
+          userId={user._id}
+          userName={user.name}
+          roomId={roomid as string}
+        />
+      </div>
     </div>
   );
 }
